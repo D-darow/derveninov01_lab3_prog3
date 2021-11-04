3e58647bd4da034f02e4babc87cc5f74006a6c73
Dima <dimader@bk.ru>
2021-11-04 18:31:47 +0700

    Added class driver and its methods

---
+++ driver.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include "driver.h"
+// Установка имени
+void driver::set_name(std::string name_in)
+{
+	name = name_in;
+}
+// Получение имени
+std::string driver::get_name()
+{
+	return std::string(name);
+}
+// Установка фамилии
+void driver::set_surname(std::string surname_in)
+{
+	surname = surname_in;
+}
+// Получение фамилии
+std::string driver::get_surname()
+{
+	return std::string(surname);
+}
+// Установка номера телефона
+void driver::set_phone_number(std::string phone_number_in)
+{
+	phone_number = phone_number_in;
+}
+// Получение номера телефона
+std::string driver::get_phone_number()
+{
+	return std::string(phone_number);
+}
+// Ввод информации о водителе
+void driver::input_driver(driver& object)
+{
+	std::cout << "Введите имя водителя: ";
+	std::cin >> name;
+	std::cout << "Введите фамилию водителя: ";
+	std::cin >> surname;
+	std::cout << "Введите номер телефона водителя: ";
+	std::cin >> phone_number;
+}
+// Вывод информации о водителе
+void driver::print_driver(driver object)
+{
+	printf("\nИнформация о водителе\n");
+	printf("Имя: %s\n", object.name);
+	printf("Фамилия: %s\n", object.surname);
+	printf("Номер телефона: %s\n\n", object.phone_number);
+	printf("Нажмите любую клавишу, чтобы вернуться в меню...");
+	_getch();
+}
---
+++ driver.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class driver
+{
+private:
+	std::string name; // Имя
+	std::string surname; // Фамилия
+	std::string phone_number; // Номер телефона
+public:
+	void set_name(std::string name_in); // Установка имени
+	std::string get_name(); // Получение имени
+	void set_surname(std::string surname_in); // Установка фамилии
+	std::string get_surname(); // Получение фамилии
+	void set_phone_number(std::string phone_number_in); // Установка номера телефона
+	std::string get_phone_number(); // Получение номера телефона
+	void input_driver(driver& object); // Ввод информации о водителе
+	void print_driver(driver object); // Вывод информации о водителе
+};
