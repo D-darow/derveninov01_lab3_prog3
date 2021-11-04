2e997b2c3d89445cb259aa6a67a610df3ab166b0
Dima <dimader@bk.ru>
2021-11-04 17:52:58 +0700

    Added class wheels and its methods

---
+++ wheels.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include "wheels.h"
+// Установка диаметра колеса
+void wheels::set_diametr_of_disk(int diametr)
+{
+	diameter_of_disk = diametr;
+}
+// Получение диаметра колеса
+int wheels::get_diametr_of_disk()
+{
+	return diameter_of_disk;
+}
+// Установка ширины шины
+void wheels::set_width_of_tire(int width)
+{
+	width_of_tire = width;
+}
+// Получение ширины шины
+int wheels::get_width_of_tire()
+{
+	return width_of_tire;
+}
+// Ввод информации о колесах
+void wheels::input_wheels(wheels& object)
+{
+	int flag = 0;
+	do {
+		if (flag == 0) printf("Введите диаметр диска (Дюйм): ");
+		if (flag == 1) printf("Ошибка. Введите диаметр диска (Дюйм): ");
+		while (scanf("%d", &object.diameter_of_disk) != 1) {
+			while (getchar() != '\n');
+			printf("Ошибка. Введите диаметр диска (Дюйм): ");
+		}
+		flag = 1;
+	} while (object.diameter_of_disk < 0);
+	flag = 0;
+	do {
+		if (flag == 0) printf("Введите ширину шины (мм): ");
+		if (flag == 1) printf("Ошибка. Введите ширину шины (мм): ");
+		while (scanf("%d", &object.width_of_tire) != 1) {
+			while (getchar() != '\n');
+			printf("Ошибка. Введите ширину шины (мм): ");
+		}
+		flag = 1;
+	} while (object.width_of_tire < 0);
+}
+// Вывод информации о колесах
+void wheels::print_wheels(wheels object)
+{
+	printf("\nИнформация о колесах\n");
+	printf("Диаметр диска (Дюйм): %d\n", object.diameter_of_disk);
+	printf("Ширина шины (мм): %d\n\n", object.width_of_tire);
+	printf("Нажмите любую клавишу, чтобы вернуться в меню...");
+	_getch();
+}
---
+++ wheels.h
@@ -0,0 +1,15 @@
+#pragma once
+class wheels
+{
+private:
+	int diameter_of_disk; // Диаметр колеса (В дюймах)
+	int width_of_tire; // Ширина шины (В мм)
+public:
+	void set_diametr_of_disk(int diametr); // Установка диаметра колеса
+	int get_diametr_of_disk(); // Получение диаметра колеса
+	void set_width_of_tire(int width); // Установка ширины шины
+	int get_width_of_tire(); // Получение ширины шины
+	void input_wheels(wheels& object); // Ввод информации о колесах
+	void print_wheels(wheels object); // Вывод информации о колесах
+};
+
