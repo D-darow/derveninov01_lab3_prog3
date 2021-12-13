1e115f582060452016a2bf4db100d83df84b51d8
Dima <dimader@bk.ru>
2021-12-13 18:32:58 +0700

    Изменена функция main для демонстрации изменений.

--- derveninov01_lab3_prog3.cpp
+++ derveninov01_lab3_prog3.cpp
@@ -7,22 +7,24 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	// Создание динамического массива объектов класса bus
-	bus* my_bus = static_cast<bus*>(operator new[](2 * sizeof(bus)));
-	for (int i = 0; i < 2; ++i) { 
-		new (my_bus + i) bus(23 + i, 0, 25);
-	}
-	// Инициализация объектов класса bus
-	for (int i = 0; i < 2; ++i) {
-		(my_bus + i)->init_bus();
-		std::cout << std::endl;
-	}
-	// Вывод объектов класса bus
-	for (int i = 0; i < 2; ++i) {
-		int bus_num_print = (my_bus + i)->get_bus_num();
-		std::cout << "Автобус номер " << bus_num_print << std::endl;
-		(my_bus + i)->print_bus();
-	}
-	// Очистка памяти
-	delete my_bus;
+	bus my_bus1 = bus(1, 0, 25);
+	my_bus1.add_passenger();
+	int* sum_by_address = my_bus1.get_sum_by_address(); // Получение суммы через указатель
+	int sum_by_link = my_bus1.get_sum_by_link(); // Получение суммы по ссылке
+	std::cout << *sum_by_address << " = " << sum_by_link << std::endl << std::endl; // Вывод значений
+	// Демонстрация постфиксной перегрузки инкремента
+	bus my_bus2 = my_bus1++;
+	int sum2 = my_bus2.get_sum_by_link();
+	int sum1 = my_bus1.get_sum_by_link();
+	std::cout << "Сумма 2 автобуса: " << sum2 << std::endl;
+	std::cout << "Сумма 1 автобуса: " << sum1 << std::endl << std::endl;
+	// Демонстрация префиксной перегрузки инкремента
+	bus my_bus3 = ++my_bus1;
+	int sum3 = my_bus3.get_sum_by_link();
+	sum1 = my_bus1.get_sum_by_link();
+	std::cout << "Сумма 3 автобуса: " << sum3 << std::endl;
+	std::cout << "Сумма 1 автобуса: " << sum1 << std::endl << std::endl;
+	// Демонстрация перегрузки оператора +
+	int sum13 = my_bus1 + my_bus3;
+	std::cout << "Сумма 3 автобуса + сумма 1 автобуса = " << sum13 << std::endl;
 }
